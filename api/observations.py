0e951879a2d5756f4052077b9934a39796b1fe03
Lukas Phaf <lukas.phaf@knmi.nl>
2024-06-03 14:38:13 +0200

    Add API skeleton for `/locations`

--- api/observations.py
+++ api/observations.py
@@ -1,9 +1,12 @@
 from typing import Annotated
 
 from covjson_pydantic.coverage import Coverage
+from covjson_pydantic.parameter import Parameter
+from edr_pydantic.parameter import EdrBaseModel
 from fastapi import APIRouter
 from fastapi import Path
 from fastapi import Query
+from geojson_pydantic import FeatureCollection
 from starlette.responses import JSONResponse
 
 
@@ -12,6 +15,32 @@ router = APIRouter(prefix="/collections/observations")
 
 class CoverageJsonResponse(JSONResponse):
     media_type = "application/prs.coverage+json"
+
+
+class GeoJsonResponse(JSONResponse):
+    media_type = "application/geo+json"
+
+
+class EDRFeatureCollection(EdrBaseModel, FeatureCollection):
+    parameters: dict[str, Parameter]
+
+
+@router.get(
+    "/locations",
+    tags=["Collection data queries"],
+    response_model=EDRFeatureCollection,
+    response_model_exclude_none=True,
+    response_class=GeoJsonResponse,
+)
+async def get_locations(
+    bbox: Annotated[str | None, Query(example="5.0,52.0,6.0,52.1")] = None,
+    datetime: Annotated[str | None, Query(example="2022-12-31T00:00Z/2023-01-01T00:00Z")] = None,
+    parameter_name: Annotated[
+        str | None,
+        Query(alias="parameter-name", description="Comma seperated list of parameter names.", example="ff, dd"),
+    ] = None,
+) -> EDRFeatureCollection:
+    pass
 
 
 @router.get(
